575abf5dc8b6e6507da30314ef6ecbe1bb638a7e
Justin O'Connor <justin.oc03@gmail.com>
2016-09-27 13:22:46 -0500

    server is up and running after app.get installed

---
+++ server/app.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var app = express();
+var path = require('path');
+var bodyParser = require('body-parser');
+var urlencodedParser = bodyParser.urlencoded({extended: false});
+var portDecision = process.env.PORT || 8888;
+
+//spin up server
+app.listen(portDecision, function (){
+  console.log('server up on', portDecision);
+});//end server up
+
+//base url
+app.get( '/', function(req, res){
+  console.log('base url hit');
+  res.sendFile( path.resolve('public/index.html'));
+}); //end base url
